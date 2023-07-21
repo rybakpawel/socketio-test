b6d1fe62d409f09d0f0f6aa2939aa22d7bfb455f
rybakpawel <71656910+rybakpawel@users.noreply.github.com>
2023-07-21 13:36:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ server.listen(port, () => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let numUsers = 0;
-
 io.on("connection", (socket) => {
-    let addedUser = false;
-
     console.log("Połączono")
 
     socket.emit("hello", "world")
@@ -30,44 +26,5 @@ io.on("connection", (socket) => {
             message: "Wiadomość odebrana",
         });
         console.log("Po Receive")
-    });
-
-    socket.on("add user", (username) => {
-        if (addedUser) return;
-
-        socket.username = username;
-        ++numUsers;
-        addedUser = true;
-        socket.emit("login", {
-            numUsers: numUsers,
-        });
-
-        socket.broadcast.emit("user joined", {
-            username: socket.username,
-            numUsers: numUsers,
-        });
-    });
-
-    socket.on("typing", () => {
-        socket.broadcast.emit("typing", {
-            username: socket.username,
-        });
-    });
-
-    socket.on("stop typing", () => {
-        socket.broadcast.emit("stop typing", {
-            username: socket.username,
-        });
-    });
-
-    socket.on("disconnect", () => {
-        if (addedUser) {
-            --numUsers;
-
-            socket.broadcast.emit("user left", {
-                username: socket.username,
-                numUsers: numUsers,
-            });
-        }
     });
 });
