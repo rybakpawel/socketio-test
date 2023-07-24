8dc15d7f3877ad371023448bfb3c66e2b788e827
rybakpawel <71656910+rybakpawel@users.noreply.github.com>
2023-07-24 13:05:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
     cors: {
@@ -12,8 +11,6 @@ const port = 5000;
 server.listen(port, () => {
     console.log("Server listening at port ", port);
 });
-
-app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
     
