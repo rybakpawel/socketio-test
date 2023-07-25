5196d4a513d2fc2114060f2b8c54f97a1fe349b6
rybakpawel <71656910+rybakpawel@users.noreply.github.com>
2023-07-25 06:44:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true,
+        origin: true,
+        credentials: true
     },
 });
 const port = 5000;
