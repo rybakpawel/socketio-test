ecc7ae2a290a355198fde516f1f55dce5c439ab0
rybakpawel <71656910+rybakpawel@users.noreply.github.com>
2023-07-26 13:55:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ server.listen(port, () => {
 });
 
 io.on("connection", socket => {
+    console.log(socket.handshake.query.userId)
+    socket.join(socket.handshake.query.userId)
     
     socket.on("send-message", data => {
-        console.log(data)
         socket.broadcast.emit("receive-message", {
             message: data.message,
             chatGroupId: data.chatGroupId
