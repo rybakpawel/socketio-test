1aa4f2b60a136a4af97ed39b5a634f9d5b751e41
rybakpawel <71656910+rybakpawel@users.noreply.github.com>
2023-07-31 13:47:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,23 @@ io.on("connection", socket => {
     socket.on('join-initial-groups', data => {
         const dataArray = JSON.parse(data);
         dataArray.forEach(data => {
-            socket.join(data.Id)
+            socket.join(data.Id);
         });
-    })
+    });
+
+    socket.on("join-group", data => {
+        const obj = JSON.parse(data);
+        
+        socket.join(obj.ChatMessage.ChatGroupId);
+    });
     
     socket.on("send-message", data => {
         const obj = JSON.parse(data);
-        console.log(obj)
+
+        if (obj.NewGroup) {
+            socket.join(obj.ChatMessage.ChatGroupId);
+            socket.broadcast.emit("join-new-group", data);
+        }
         socket.broadcast.to(obj.ChatMessage.ChatGroupId).emit("receive-message", data);
     });
 });
