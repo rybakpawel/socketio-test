5fd18bf946187f6ba5d146a6730d31d124dd66c5
rybakpawel <71656910+rybakpawel@users.noreply.github.com>
2023-08-09 09:26:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*"
+        origin: "https://skillandchill-dev.outsystemsenterprise.com/"
     },
 });
 const port = 5000;
