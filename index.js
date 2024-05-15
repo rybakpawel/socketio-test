bfa54d282254e7ef4c34a40dfe3f20c6d2d81fca
rybakpawel <71656910+rybakpawel@users.noreply.github.com>
2024-05-15 08:41:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ io.on("connection", socket => {
     console.log("Użytkownik " + userId + " został zalogowany.");
     console.log("Zalogowani użytkownicy: " + Array.from(connectedUsers));
 
-    getAllSockets();
+    // getAllSockets();
     console.log(socket.handshake.query.userId)
     socket.broadcast.emit('receive-connected-users', data);
     // io.emit('receive-connected-users', data);
